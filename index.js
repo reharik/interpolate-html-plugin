5a1cb16de88c7c28de63c9e5b67060872c4c9de0
Raif Harik <reharik@gmail.com>
2018-07-07 14:05:02 -0500

    webpack4 compatible

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-'use strict'
-
-/*
- * The code is almost copied directly from
- * https://github.com/facebookincubator/create-react-app/blob/d1250743adc2abc41d80d566c5f817e1a16da279/packages/react-dev-utils/InterpolateHtmlPlugin.js
- */
-
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 // This Webpack plugin lets us interpolate custom variables into `index.html`.
@@ -22,12 +13,12 @@
 // Learn more about creating plugins like this:
 // https://github.com/ampedandwired/html-webpack-plugin#events
 
-'use strict'
-const escapeStringRegexp = require('escape-string-regexp')
+'use strict';
+const escapeStringRegexp = require('escape-string-regexp');
 
 class InterpolateHtmlPlugin {
   constructor(replacements) {
-    this.replacements = replacements
+    this.replacements = replacements;
   }
 
   apply(compiler) {
@@ -37,16 +28,16 @@ class InterpolateHtmlPlugin {
         data => {
           // Run HTML through a series of user-specified string replacements.
           Object.keys(this.replacements).forEach(key => {
-            const value = this.replacements[key]
+            const value = this.replacements[key];
             data.html = data.html.replace(
               new RegExp('%' + escapeStringRegexp(key) + '%', 'g'),
               value
-            )
-          })
+            );
+          });
         }
-      )
-    })
+      );
+    });
   }
 }
 
-module.exports = InterpolateHtmlPlugin
+module.exports = InterpolateHtmlPlugin;
